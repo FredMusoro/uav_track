41ff7c3984494f12ad2b83933c31795b5ebc499f
Andrzej <andrzmichal@gmail.com>
2019-02-13 22:16:11 +0100

    Add feature start point

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
-from funcs import read_track_data
+from funcs import read_track_data, read_base_points
 
 app = Flask(__name__)
 
 @app.route('/')
 def track():
     points = read_track_data()
+    start_points, land_points = read_base_points()
     lat_lon = [[point[1], point[2]] for point in points]
-    return render_template('index.html', lat_lon=lat_lon)
+    return render_template('index.html', lat_lon=lat_lon, start_points=start_points, land_points=land_points)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- funcs.py
+++ funcs.py
@@ -7,3 +7,11 @@ def read_track_data(file='track.json'):
         parsed_data = json.loads(data)
         route_points = parsed_data['route_points']
         return route_points
+
+def read_base_points(file='base_points.json'):
+    with open(file, 'r') as f:
+        data  = f.read()
+        parsed_data = json.loads(data)
+        start_points = parsed_data['start_points']
+        land_points = parsed_data['land_points']
+        return start_points, land_points
