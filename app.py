fd67020cba623583edf6b5c8df9fa310a9ccd7e1
Andrzej <andrzmichal@gmail.com>
2019-02-14 11:15:52 +0100

    Export start, end point, height

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from funcs import read_track_data, read_base_points
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def track():
+    if request.method == 'POST':
+        track_data = request.json
+        print(track_data)
+
     points = read_track_data()
     start_points, land_points = read_base_points()
     lat_lon = [[point[1], point[2]] for point in points]
